4e2f0673c5afc847a7e7e8628a2ec5ea83d9d6b9
BTrDung <45888776+BTrDung@users.noreply.github.com>
2020-04-24 22:23:34 +0700

    Add files via upload

---
+++ Predict_Number_Mnist.py
@@ -0,0 +1,14 @@
+import PIL
+import collections
+import numpy as np
+import tensorflow as tf
+import imageio
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+
+model = load_model("TestModel.h5")
+Test_images = LoadIMG = imageio.imread("TestIMG")
+Test_images = Test_images.reshape(1, 28, 28, 1)
+Test_images = Test_images / 225
+predict_number = model.predict(Test_images)
+print(predict_number.argmax())
