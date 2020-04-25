88f0acf68cfe6875c98bab458ef219084672c4a4
BTrDung <45888776+BTrDung@users.noreply.github.com>
2020-04-25 19:36:07 +0700

    Update Predict_Number_Mnist.py

--- Predict_Number_Mnist.py
+++ Predict_Number_Mnist.py
@@ -1,4 +1,5 @@
 import PIL
+import os
 import collections
 import numpy as np
 import tensorflow as tf
@@ -6,8 +7,8 @@ import imageio
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 
-model = load_model("TestModel.h5")
-Test_images = LoadIMG = imageio.imread("TestIMG")
+model = load_model("TestMyModel.h5")
+Test_images = LoadIMG = imageio.imread("_________________")
 Test_images = Test_images.reshape(1, 28, 28, 1)
 Test_images = Test_images / 225
 predict_number = model.predict(Test_images)
